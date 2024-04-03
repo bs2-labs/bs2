f362a820e09c23688da24365bbad82eb426e8fb6
Harry <2411mail@gmail.com>
2024-04-03 23:02:15 +0800

    update constraint and assign method for other type

--- circuits/src/execution_table/op_configure/others.rs
+++ circuits/src/execution_table/op_configure/others.rs
@@ -32,15 +32,6 @@ impl<F: FieldExt> OthersTypeGadget<F> {
 
         // todo: constrain selector: s1 + s1 + .. + sn = 1
 
-        cs.create_gate("OthersType::FENCE", |vc| {
-            let lhs = vc.query_advice(lhs_col, Rotation::cur());
-            let rhs = vc.query_advice(rhs_col, Rotation::cur());
-            let out = vc.query_advice(lhs_col, Rotation::next());
-            let s = vc.query_selector(s_fence);
-            // let (value, _) = rs1_value.overflowing_sub(rs2_value);
-            vec![s * (lhs + rhs - out)]
-        });
-
         Self {
             lhs_col,
             rhs_col,
@@ -53,35 +44,6 @@ impl<F: FieldExt> OthersTypeGadget<F> {
         layouter.assign_region(
             || "OthersType",
             |mut region| {
-                // todo
-                let rs1 = step.instruction.op_b;
-                let rs2 = step.instruction.op_c;
-                let rd = step.instruction.op_a;
-                let rs1_value = step.register_indexes.unwrap().read(rs1).unwrap();
-                let rs2_value = step.register_indexes.unwrap().read(rs2).unwrap();
-                let rd_value = step.register_indexes.unwrap().read(rd).unwrap();
-
-                region.assign_advice(
-                    || "lhs",
-                    self.lhs_col,
-                    0,
-                    || Value::known(F::from(rs1_value)),
-                )?;
-
-                region.assign_advice(
-                    || "rhs",
-                    self.rhs_col,
-                    0,
-                    || Value::known(F::from(rs2_value)),
-                )?;
-
-                region.assign_advice(
-                    || "output",
-                    self.lhs_col,
-                    1,
-                    || Value::known(F::from(rd_value)),
-                )?;
-
                 match step.instruction.opcode.into() {
                     Opcode::FENCE => self.s_fence.enable(&mut region, 0)?,
                     _ => panic!("Not implemented {:?}", step.instruction.opcode),
