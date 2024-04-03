c28f289097bb5294d86b87ddcddc9a767bf2f291
Harry <2411mail@gmail.com>
2024-04-03 12:05:56 +0800

    feat: add constraint for addition

--- circuits/src/execution_table/op_configure/rtype.rs
+++ circuits/src/execution_table/op_configure/rtype.rs
@@ -1,42 +1,58 @@
 use crate::execution_table::Entries;
 use core::marker::PhantomData;
+use std::iter::StepBy;
 use halo2_proofs::arithmetic::{FieldExt, Field};
 use halo2_proofs::circuit::{AssignedCell, Value, Layouter, SimpleFloorPlanner};
 use halo2_proofs::halo2curves::bn256::Fr;
 use halo2_proofs::plonk::*;
 use halo2_proofs::poly::Rotation;
+use runtime::trace::{Step, InstructionType, BType, IType, JType, NoType, RType, SType, UType};
 
 #[derive(Debug, Clone)]
 pub struct ACell<F: FieldExt>(pub AssignedCell<F, F>);
 
 #[derive(Clone)]
 pub struct RTypeGadget<F> {
-    pub col0: Column<Advice>,
-    pub col1: Column<Advice>,
-    selector: Selector,
+    pub lhs_col: Column<Advice>,
+    pub rhs_col: Column<Advice>,
+    s_add: Selector,
+    s_sub: Selector,
     _maker: PhantomData<F>,
 }
 
 impl<F: FieldExt> RTypeGadget<F> {
     pub fn configure(cs: &mut ConstraintSystem<F>) -> Self {
-        let col0 = cs.advice_column();
-        let col1 = cs.advice_column();
-        let selector = cs.selector();
-        cs.enable_equality(col0);
-        cs.enable_equality(col1);
+        let lhs_col = cs.advice_column();
+        let rhs_col = cs.advice_column();
+        // todo: add selector for every gate
+        let s_add = cs.selector();
+        let s_sub = cs.selector();
+        cs.enable_equality(lhs_col);
+        cs.enable_equality(rhs_col);
 
-        cs.create_gate("add", |cs| {
-            let lhs = cs.query_advice(col0, Rotation::cur());
-            let rhs = cs.query_advice(col1, Rotation::cur());
-            let out = cs.query_advice(col0, Rotation::next());
-            let s = cs.query_selector(selector);
+
+        // todo: constrain selector: s1 + s1 + .. + sn = 1
+        cs.create_gate("add", |vc| {
+            let lhs = vc.query_advice(lhs_col, Rotation::cur());
+            let rhs = vc.query_advice(rhs_col, Rotation::cur());
+            let out = vc.query_advice(lhs_col, Rotation::next());
+            let s = vc.query_selector(s_add);
             vec![s * (lhs + rhs - out)]
         });
 
+        cs.create_gate("sub", |vc| {
+            let lhs = vc.query_advice(lhs_col, Rotation::cur());
+            let rhs = vc.query_advice(rhs_col, Rotation::cur());
+            let out = vc.query_advice(lhs_col, Rotation::next());
+            let s = vc.query_selector(s_sub);
+            vec![s * (lhs - rhs - out)]
+        });
+
         Self {
-            col0,
-            col1,
-            selector,
+            lhs_col,
+            rhs_col,
+            s_add,
+            s_sub,
             _maker: PhantomData::default(),
         }
     }
@@ -44,31 +60,44 @@ impl<F: FieldExt> RTypeGadget<F> {
     pub fn assign(
         &self,
         layouter: &mut impl Layouter<F>,
+        // step: &Step,
         entries: &Entries,
     ) -> Result<(), Error> {
+
         layouter.assign_region(
-            || "fibo",
+            || "rtype",
             |mut region| {
-                let selector = self.selector;
-                selector.enable(&mut region, 0)?;
+                // rs1_value
+                // let col0 = step.registers[step.instruction.op_b as usize];
+                // // rs2_value
+                // let col1 = step.registers[step.instruction.op_c as usize];
+                // // rd_value at next row
+                // let col0_next = step.registers[step.instruction.op_a as usize];
+                // let col0 = self.col0;
+                // let col1 = self.col1;
+                // todo: assign selector value to some position
+                let s_add = self.s_add;
+                // let s_sub = self.s_sub;
+                s_add.enable(&mut region, 0)?;
+                // s_sub.enable(&mut region, 0)?;
 
                 region.assign_advice(
                     || "lhs",
-                    self.col0,
+                    self.lhs_col,
                     0,
                     || Value::known(F::from(100)),
                 )?;
 
                 region.assign_advice(
-                    || "private input",
-                    self.col1,
+                    || "rhs",
+                    self.rhs_col,
                     0,
                     || Value::known(F::from(20)),
                 )?;
 
                 region.assign_advice(
-                    || "private input",
-                    self.col0,
+                    || "output",
+                    self.lhs_col,
                     1,
                     || Value::known(F::from(120)),
                 )?;
